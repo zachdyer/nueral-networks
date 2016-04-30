9b4aa7deb36920f5c2626ae3adfce3dce85186f4
Zach Dyer <zach.dyer@zachdyerdesign.com>
2016-04-30 18:03:22 -0500

    cleaned up code

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ this.network = new synaptic.Architect.Perceptron(40, 25, 3);
 
 var A = new synaptic.Neuron();
 var B = new synaptic.Neuron();
-var C = new synaptic.Neuron();
+
 A.project(B); // A now projects a connection to B
-B.project(C);
 
 var learningRate = 0.3;
 
+// Train nueron B 10 times at a learning rate of 0.3
 for(var i = 0; i < 10; i++)
 {
     // when A activates 1
@@ -18,13 +18,3 @@ for(var i = 0; i < 10; i++)
     console.log(B.activate());
     B.propagate(learningRate, 0);
 }
-
-// test it
-/*
-console.log("A.activate(1)");
-console.log(A.activate(1));
-console.log("B.activate()");
-console.log(B.activate()); // 0.006540565760853365
-*/
-
-//console.log(A);
